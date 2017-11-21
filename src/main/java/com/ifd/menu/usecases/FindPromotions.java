c35c2f37bdb0d201075d5f77a4f5530bb1907e22
Guilherme <guilherme.roveri@netshoes.com>
2017-11-21 09:02:33 -0200

    hooking find promotions

--- src/main/java/com/ifd/menu/usecases/FindPromotions.java
+++ src/main/java/com/ifd/menu/usecases/FindPromotions.java
@@ -2,12 +2,17 @@ package com.ifd.menu.usecases;
 
 import com.ifd.menu.domains.IfdContext;
 import com.ifd.menu.domains.Promotion;
+import com.ifd.menu.gateways.PromotionGateway;
 import io.reactivex.Observable;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class FindPromotions {
+    private final PromotionGateway promotionGateway;
+
     public Observable<Promotion> execute(final String restaurantId, IfdContext context) {
-        return null;
+        return promotionGateway.findPromotion(restaurantId, context);
     }
 }
---
+++ src/test/groovy/com/ifd/menu/usecases/FindPromotionsSpec.groovy
@@ -0,0 +1,25 @@
+package com.ifd.menu.usecases
+
+import com.ifd.menu.domains.IfdContext
+import com.ifd.menu.domains.Promotion
+import com.ifd.menu.gateways.PromotionGateway
+import io.reactivex.Observable
+import spock.lang.Specification
+
+class FindPromotionsSpec extends Specification {
+
+    def promotionGateway = Mock(PromotionGateway)
+    def findPromotions = new FindPromotions(promotionGateway)
+
+    def "find promotion with success"() {
+        given: "exists default_promo with 10% off for restaurant burger_n_chips"
+          def defaultPromo = new Promotion(discount: 10, id: "default_promo")
+          promotionGateway.findPromotion(_, _) >> Observable.just(defaultPromo)
+        when: "find promotions for restaurant burger_n_chips"
+          def promo = findPromotions.execute("burger_n_chips", new IfdContext())
+        then: "promo default_promo with 10% off is returned"
+          def testObserver = promo.test().assertComplete().assertNoErrors()
+          testObserver.values()[0].discount == 10
+    }
+
+}
